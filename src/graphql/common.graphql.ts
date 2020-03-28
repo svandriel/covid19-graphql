b454eca1363d961a1d8fc0e20fbf17d1d7db9da4
Sander van Driel <sander.vandriel@gmail.com>
2020-03-28 22:26:57 +0100

    feat: finished aggregation

--- src/graphql/common.graphql.ts
+++ src/graphql/common.graphql.ts
@@ -5,9 +5,9 @@ export const typeDefs = gql`
 
     type Query {
         """
-        Aggregates all data into a single global timeline.
+        Aggregates multiple timelines into a single one.
         """
-        globalTimeline(
+        timeline(
             """
             Filters time line items to be on or after this date (inclusive)
             """
@@ -16,6 +16,30 @@ export const typeDefs = gql`
             Filters time line items to be before this date (exclusive)
             """
             to: LocalDate
+            """
+            Determines which regions to include (default: all)
+            """
+            regions: [String!]
+            """
+            Determines which regions to exclude (default: none)
+            """
+            excludeRegions: [String!]
+            """
+            Determines which sub-regions to include (default: all)
+            """
+            subRegions: [String!]
+            """
+            Determines which sub-regions to exclude (default: none)
+            """
+            excludeSubRegions: [String!]
+            """
+            Determines which countries to include (default: all)
+            """
+            countries: [String!]
+            """
+            Determines which countries to exclude (default: none)
+            """
+            excludeCountries: [String!]
         ): [TimelineItem!]!
     }
 
@@ -25,5 +49,16 @@ export const typeDefs = gql`
         deceased: Int!
         recovered: Int!
         lastUpdated: String
+    }
+
+    input IncludeExclude {
+        """
+        List of items to include (default: all)
+        """
+        include: [String!]
+        """
+        List of items to exclude (default: none)
+        """
+        exclude: [String!]
     }
 `;
--- src/graphql/common.ts
+++ src/graphql/common.ts
@@ -1,13 +1,69 @@
 import { Moment } from 'moment';
-import { isNil } from 'ramda';
+import { allPass, isNil, pluck, uniq } from 'ramda';
 
+import { getCountryLookup } from '../country-lookup';
 import { ApiResolvers, ApiTimelineItem } from '../generated/graphql-backend';
+import { TimeSeries } from '../types/time-series-item';
+import { makeStringLookup } from '../util/make-lookup';
 import { LocalDate } from './custom-scalars/local-date';
 
 export const resolvers: ApiResolvers = {
     Query: {
-        globalTimeline: async (_query, { from, to }, context) => {
-            const stats = await context.dataSource.getGlobalTimeSeriesFromCsv();
+        timeline: async (_query, args, context) => {
+            const lookup = await getCountryLookup();
+            const {
+                from,
+                to,
+                regions,
+                excludeRegions,
+                subRegions,
+                excludeSubRegions,
+                countries,
+                excludeCountries,
+            } = args;
+
+            const filters: Array<(item: TimeSeries) => boolean> = [];
+            if (regions) {
+                if (regions) {
+                    const includedCountries = uniq(regions).flatMap(
+                        includedRegion => lookup.countriesPerRegion[includedRegion],
+                    );
+                    const countryLookup = makeStringLookup(pluck('code', includedCountries));
+                    filters.push(item => !!countryLookup[item.countryCode]);
+                }
+                if (excludeRegions) {
+                    const excludedCountries = uniq(excludeRegions).flatMap(
+                        excludedRegion => lookup.countriesPerRegion[excludedRegion],
+                    );
+                    const countryLookup = makeStringLookup(pluck('code', excludedCountries));
+                    filters.push(item => !countryLookup[item.countryCode]);
+                }
+            }
+            if (subRegions) {
+                const includedCountries = uniq(subRegions).flatMap(
+                    includedSubRegion => lookup.countriesPerSubRegion[includedSubRegion],
+                );
+                const countryLookup = makeStringLookup(pluck('code', includedCountries));
+                filters.push(item => !!countryLookup[item.countryCode]);
+            }
+            if (excludeSubRegions) {
+                const excludedCountries = uniq(excludeSubRegions).flatMap(
+                    excludedSubRegion => lookup.countriesPerSubRegion[excludedSubRegion],
+                );
+                const countryLookup = makeStringLookup(pluck('code', excludedCountries));
+                filters.push(item => !countryLookup[item.countryCode]);
+            }
+            if (countries) {
+                const includedCountries = uniq(countries);
+                const countryLookup = makeStringLookup(includedCountries);
+                filters.push(item => !!countryLookup[item.countryCode]);
+            }
+            if (excludeCountries) {
+                const excludedCountries = uniq(excludeCountries);
+                const countryLookup = makeStringLookup(excludedCountries);
+                filters.push(item => !countryLookup[item.countryCode]);
+            }
+            const stats = await context.dataSource.getAggregatedTimelineFromCsv(allPass(filters));
             return applyTimeSeriesRange({ from, to } || {}, stats);
         },
     },
